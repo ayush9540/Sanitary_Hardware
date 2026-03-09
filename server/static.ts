8605e6bc6be68d265f3e19be073de88e6e351233
Ayush Gupta <018.ayush@gmail.com>
2026-03-09 21:42:26 +0530

    static file updated

--- server/static.ts
+++ server/static.ts
@@ -1,6 +1,9 @@
 import express, { type Express } from "express";
-import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import fs from "fs";
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
